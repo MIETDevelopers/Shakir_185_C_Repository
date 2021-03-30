8535395cf0fd573b10460d1c13471da10904c5fa
shakirhussain-github <2020a1r185@mietjammu.in>
2021-03-31 00:18:06 +0530

    hello world program

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
-int main(){
-	printf("HELLO WORLD\n");
-	return 0;
+// Author: Shakir Hussain Itoo
+// Creation Date: 27 Jan 2021
+// Purpose: C program to print  Hello World
+#include <stdio.h> // The #include is a command which tells the compiler to include the contents of standard input and output header file in the program.
+// The stdio.h file contains functions such as scanf() and printf() to take input and display output respectively.
+// The execution of a Cprogram starts from the main() function.
+int main() {      
+    printf("Hello world,\n");    // printf() is a library function to display the formatted output on the screen. in this program, printf() displays Hello World text on the screen.
+    // The return 0 statement is the "EXIT STATUS" of the program. Simply i can say program ends with this statement.     
+    return 0;
 }
+	
