ca246517146ed41299f80aa909836606e99d30b1
shakirhussain-github <2020a1r185@mietjammu.in>
2021-03-31 21:29:54 +0530

    Armstrong or not while using the while loop

---
+++ 15_Armstrong.c
@@ -0,0 +1,30 @@
+// Author: Shakir Hussain 
+// Creation Date: Mar 2021
+// Purpose: Program to check if the no. is armstrong or not using while loop.
+#include <stdio.h>
+//The #include is a command which tells the compiler to include the contents of standard input and output header file in the program.
+// The stdio.h file contains functions such as scanf() and printf() to take input and display output respectively.
+// The execution of a Cprogram starts from the main() function.
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+       // remainder contains the last digit
+        remainder = originalNum % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the orignal number
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
