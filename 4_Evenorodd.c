15b8cc679cc9ee586638a32c1511be7a0ce9ca78
shakirhussain-github <2020a1r185@mietjammu.in>
2021-03-31 16:50:02 +0530

    uploaded  4_Evenorodd.c

---
+++ 4_Evenorodd.c
@@ -0,0 +1,17 @@
+// Author: Shakir Hussain Itoo#include<stdio.h>  
+// Creation Date: 27 Jan 2021
+// Purpose: Program to check whrther the no. is even or odd. 
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // True if num is perfectly divisible by 2
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+    
+    return 0;
+}
