c5e405912f4fe781305c5e2b61847c9fce8255e3
shakirhussain-github <2020a1r185@mietjammu.in>
2021-04-02 12:59:48 +0530

    Euclidean assignment file

---
+++ Assignments/Assignments.c
@@ -0,0 +1,11 @@
+// Author: Shakir Hussain Itoo#include<stdio.h>  
+// Creation Date: 27 Jan 2021
+// Purpose: C program to print  Hello World
+#include <stdio.h> // The #include is a command which tells the compiler to include the contents of standard input and output header file in the program.
+// The stdio.h file contains functions such as scanf() and printf() to take input and display output respectively.
+// The execution of a Cprogram starts from the main() function.
+int main() {      
+    printf("Hello world,\n");    // printf() is a library function to display the formatted output on the screen. in this program, printf() displays Hello World text on the screen.
+    // The return 0 statement is the "EXIT STATUS" of the program. Simply i can say program ends with this statement.     
+    return 0;
+}
