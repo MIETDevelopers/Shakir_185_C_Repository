3f7389d49c2b91c1cfc8cfdfd641ea0bf54a4411
shakirhussain-github <2020a1r185@mietjammu.in>
2021-04-02 09:36:53 +0530

    Function implementation which returns a pointer

---
+++ 42_Returningpointer.c
@@ -0,0 +1,27 @@
+// Author: Shakir Hussain Itoo
+// Creation Date:  March 2021
+#include <stdio.h>
+// Function that returns pointer
+int* fun()
+{
+    // Declare a static integer
+    static int A = 10;
+    return (&A);
+}
+
+// Driver Code
+int main()
+{
+    // Declare a pointer
+    int* p;
+
+    // Function call
+    p = fun();
+
+    // Print Address
+    printf("%p\n", p);
+
+    // Print value at the above address
+    printf("%d\n", *p);
+    return 0;
+} 
