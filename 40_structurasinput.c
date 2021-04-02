ddfb5e21a774e40d47d31ff5fd3d004c21c3f33e
shakirhussain-github <2020a1r185@mietjammu.in>
2021-04-02 09:23:28 +0530

    fuction which takes structure as input

---
+++ 40_structurasinput.c
@@ -0,0 +1,30 @@
+// Author: Shakir Hussain Itoo
+// Creation Date:  March 2021
+// Purpose: C Program to structure as an input.
+#include<stdio.h>  //Pre-process directive to include standard input and output functions header file
+
+/*
+structure is defined above all functions so it is global.*/
+struct student
+{
+    char name[20];
+    int roll_no;
+    int marks;
+};
+
+void print_struct(char name[], int roll_no, int marks);  //declaration
+
+int main()   //main function
+{
+    struct student stu = {"Tim", 1, 78};  
+    print_struct(stu.name, stu.roll_no, stu.marks);
+    return 0;  //return function
+}
+
+void print_struct(char name[], int roll_no, int marks)
+{
+    printf("Name: %s\n", name);   //printf function calling
+    printf("Roll no: %d\n", roll_no);
+    printf("Marks: %d\n", marks);
+    printf("\n");
+} 
